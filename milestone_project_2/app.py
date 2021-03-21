8ace3933a2bd8b0e8711d66789d25da33a447339
David Bruce <david.bruce14@gmail.com>
2021-03-21 17:26:14 -0400

    small change to list_books function

--- milestone_project_2/app.py
+++ milestone_project_2/app.py
@@ -40,7 +40,8 @@ def prompt_add_book():
 def list_books():
     books = database.get_all_books()
     for book in books:
-        print(f"{book['name']} by {book['author']}, read: {book['read']}")
+        read = 'YES' if book['read'] else 'NO'
+        print(f"{book['name']} by {book['author']}, read: {read}")
 
 
 # def prompt_read_book() ask for book name and change it to "read" in our list
