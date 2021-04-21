a1d8997f106624c40c13e54d6ce4401e8144c9e8
David Bruce <david.bruce14@gmail.com>
2021-04-21 16:59:08 -0400

    add pagination

--- milestone_project_4/app.py
+++ milestone_project_4/app.py
@@ -7,5 +7,8 @@ page = AllBooksPage(page_content)
 
 books = page.books
 
-for book in books:
-    print(book)
+for page_num in range(1, page.page_count):
+    url = f'http://books.toscrape.com/catalogue/page-{page_num+1}.html'
+    page_content = requests.get(url).content
+    page = AllBooksPage(page_content)
+    books.extend(page.books)
