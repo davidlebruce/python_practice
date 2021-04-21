fe22378de4a60b3d23fb5056bbc1532b87fd011b
David Bruce <david.bruce14@gmail.com>
2021-04-21 17:34:23 -0400

    add logging

--- milestone_project_4/menu.py
+++ milestone_project_4/menu.py
@@ -1,4 +1,8 @@
+import logging
+
 from app import books
+
+logger = logging.getLogger('scraping.menu')
 
 
 USER_CHOICE = '''Enter one of the following
@@ -12,12 +16,14 @@ Enter your choice: '''
 
 
 def print_best_books():
+    logger.info('Finding best books by rating...')
     best_books = sorted(books, key=lambda x: (x.rating * -1, x.price))[:10] # sorts by rating DESC, then by price ASC
     for book in best_books:
         print(book)
 
 
 def print_cheapest_books():
+    logger.info('Finding best books by price...')
     cheapest_books = sorted(books, key=lambda x: x.price)[:10]
     for book in cheapest_books:
         print(book)
@@ -27,6 +33,7 @@ books_generator = (x for x in books)
 
 
 def get_next_book():
+    logger.info('Getting next book...')
     print(next(books_generator))
 
 
@@ -44,6 +51,7 @@ def menu():
         else:
             print('Please choose a valid command.')
         user_input = input(USER_CHOICE)
+    logger.debug('Terminating program...')
 
 
 menu()
